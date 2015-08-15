1c010d487bd6bae9d92a8980a438bb62d1b438e7
SLoW <snollygolly@gmail.com>
2015-08-14 23:28:24 -0500

    added chosen sets and interactivity

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,18 @@
+//the deck the game is dealt off of
 var deck = [];
+//the deck the cards are discarded into
 var discard = [];
+//the sets available
+var setsAvailable = [];
+//the sets we've chosen to make up our deck
+var setsChosen = [];
 
 $( document ).ready(function() {
   debug("Loaded!");
   fetchAllSets();
+  //make event listeners
+  $(document).on("click","#sets-available li", availableSetsClicked);
+  $(document).on("click","#sets-chosen li", chosenSetsClicked);
 });
 
 function debug(message){
@@ -40,12 +49,50 @@ function buildDeck(deck, sets){
 }
 
 function fetchAllSets(){
+  setsAvailable = [];
+  setsChosen = [];
   $.getJSON( "/js/cards.json", function( data ) {
     debug("Set index loaded. " + data.sets.length + " total sets.");
-    var i = 0;
-    while (i < data.sets.length){
-      $("#sets-available").append("<li>" + data.sets[i].name + "</li>");
-      i++;
-    }
+    setsAvailable = data.sets;
+    drawSets();
   });
+}
+
+function drawSets(){
+  $("#sets-available").html("");
+  var i = 0;
+  while (i < setsAvailable.length){
+    $("#sets-available").append("<li>" + setsAvailable[i].name + "</li>");
+    i++;
+  }
+  $("#sets-chosen").html("");
+  var i = 0;
+  var difficulty = 0;
+  while (i < setsChosen.length){
+    $("#sets-chosen").append("<li>" + setsChosen[i].name + "</li>");
+    difficulty += setsChosen[i].difficulty;
+    i++;
+  }
+  if (setsChosen.length != 0){
+    difficulty /= setsChosen.length;
+    $("#difficulty").html("Average Difficulty: <b>" + Math.round(difficulty * 100) / 100 + "</b>");
+  }else{
+    $("#difficulty").html("");
+  }
+}
+
+function availableSetsClicked(e){
+  var index = $(this).index();
+  setsChosen.push(setsAvailable[index]);
+  $("#sets-chosen").append("<li>" + setsAvailable[index].name + "</li>");
+  setsAvailable.splice(index, 1);
+  drawSets();
+}
+
+function chosenSetsClicked(e){
+  var index = $(this).index();
+  setsAvailable.push(setsChosen[index]);
+  $("#sets-available").append("<li>" + setsChosen[index].name + "</li>");
+  setsChosen.splice(index, 1);
+  drawSets();
 }
